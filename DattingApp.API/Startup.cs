8ebf950c58614d6f3f68c0fdc49dbf582fb6f4ee
sebasakd86 <narbaysebas@gmail.com>
2019-12-19 12:15:07 -0800

    Global error handling

--- DattingApp.API/Startup.cs
+++ DattingApp.API/Startup.cs
@@ -14,6 +14,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DattingApp.API.Helpers;
+
 namespace DattingApp.API
 {
     public class Startup
@@ -57,6 +62,20 @@ namespace DattingApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             app.UseHttpsRedirection();
