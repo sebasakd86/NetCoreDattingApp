299597efe1d14550d489f82d935ada8a6fae7a74
sebasakd86 <narbaysebas@gmail.com>
2020-01-10 16:27:46 -0800

    Added Validation for the register component up to the date handling Module

--- DattingApp-SPA/src/app/app.module.ts
+++ DattingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -62,6 +62,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
--- DattingApp-SPA/src/app/register/register.component.ts
+++ DattingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
 import { AlertifyService } from '../_service/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,9 +12,43 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any; // To pass down parameters from the parent.
   @Output() cancelRegister = new EventEmitter(); // Emit an event for the parent to capture and act upon it.
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
-  ngOnInit() {}
+  registerForm: FormGroup;
+
+
+  constructor(private authService: AuthService,
+              private alertify: AlertifyService,
+              private fb: FormBuilder) { }
+  ngOnInit() {
+    /*
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }, this.passwordMatchValidator);
+    */
+   this.createRegisterForm();
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {mismatch: true};
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator});
+  }
+// , Validators.minLength(4), Validators.maxLength(8)]],
   register() {
+    /*
     this.authService.register(this.model).subscribe(() => {
       // console.log('registration ok');
       this.alertify.success('Registration Succesfull');
@@ -21,6 +56,8 @@ export class RegisterComponent implements OnInit {
       // console.log('error registering!', error);
       this.alertify.error('Error registering');
     });
+    */
+   console.log(this.registerForm.value);
   }
   cancel() {
     this.cancelRegister.emit(false);
